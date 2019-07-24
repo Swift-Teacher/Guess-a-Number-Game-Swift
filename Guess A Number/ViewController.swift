7455a7cf0f0322c508ee992f0d082803b1f0e734
lillianseay <lseay55@gmail.com>
2019-07-24 10:46:18 -0700

    Initial Commit

---
+++ Guess A Number/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess A Number
+//
+//  Created by Lillian Seay on 7/24/19.
+//  Copyright © 2019 JavaSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Guess A NumberTests/Guess_A_NumberTests.swift
@@ -0,0 +1,34 @@
+//
+//  Guess_A_NumberTests.swift
+//  Guess A NumberTests
+//
+//  Created by Lillian Seay on 7/24/19.
+//  Copyright © 2019 JavaSwift. All rights reserved.
+//
+
+import XCTest
+@testable import Guess_A_Number
+
+class Guess_A_NumberTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
